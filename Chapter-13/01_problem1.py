fd8f5db5e668cbb39bcb884464a25aaab513c30a
ANISH CHAKRABORTY <anishchakraborty35@gmail.com>
2026-05-25 01:02:47 +0530

    commit

---
+++ Chapter-13/01_problem1.py
@@ -0,0 +1,26 @@
+# create virtual environment in powersheel
+
+# virtualenv env1
+
+# activate environment in powersheel 
+
+# .\env1\Scripts\Activate.ps1
+
+# create virtual environment in powersheel
+
+# virtualenv env2
+
+# activate environment in powersheel 
+
+# .\env2\Scripts\Activate.ps1
+
+
+# create requirement.txt file
+
+# pip freeze>requirement.txt
+
+# requirement file install
+
+# pip install -r .\requirement.txt
+# or
+# python -m pip install -r .\requirement.txt
